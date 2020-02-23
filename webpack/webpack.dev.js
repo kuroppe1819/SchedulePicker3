ef4377a8260f4eb4f8d32beb4db37f6b57be8c36
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-02-23 11:02:51 +0900

    バンドルサイズの測定を行うプラグインを導入する

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge'); // eslint-disable-line
 const common = require('./webpack.common.js'); // eslint-disable-line
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; // eslint-disable-line
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     mode: 'development',
+    plugins: [new BundleAnalyzerPlugin()],
 });
