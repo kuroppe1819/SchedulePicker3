20ceafcb2f524c97bff4e84172d081fe27ab276b
kuroppe <kuroppems@gmail.com>
2020-02-29 22:58:47 +0900

    フィルター設定の枠を追加

--- src/popup/IndexMain.tsx
+++ src/popup/IndexMain.tsx
@@ -37,30 +37,32 @@ export const IndexMain: React.FC = () => {
                 </Box>
             </PopupHeader>
             <PopupContainer maxWidth="false" padding={0}>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={filterState.isIncludePrivateEvent}
-                            onChange={handleChangeSwitch('isIncludePrivateEvent')}
-                            value="PrivateEventFilter"
-                            color="primary"
-                        />
-                    }
-                    labelPlacement="end"
-                    label="非公開予定を含む"
-                />
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={filterState.isIncludeAllDayEvent}
-                            onChange={handleChangeSwitch('isIncludeAllDayEvent')}
-                            value="AllDayEventFilter"
-                            color="primary"
-                        />
-                    }
-                    labelPlacement="end"
-                    label="終日予定を含む"
-                />
+                <FilterSettingFrame>
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={filterState.isIncludePrivateEvent}
+                                onChange={handleChangeSwitch('isIncludePrivateEvent')}
+                                value="PrivateEventFilter"
+                                color="primary"
+                            />
+                        }
+                        labelPlacement="end"
+                        label="非公開予定を含む"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={filterState.isIncludeAllDayEvent}
+                                onChange={handleChangeSwitch('isIncludeAllDayEvent')}
+                                value="AllDayEventFilter"
+                                color="primary"
+                            />
+                        }
+                        labelPlacement="end"
+                        label="終日予定を含む"
+                    />
+                </FilterSettingFrame>
                 <TextField
                     className="template-multiline-text"
                     label="テンプレート"
@@ -95,4 +97,24 @@ const PopupHeader = styled(Box)`
 const PopupContainer = styled(Container)`
     min-width: 360px;
     height: 800px;
+    background-color: ${grey[50]};
+`;
+
+const FilterSettingFrame = styled(Box)`
+    margin: 24px 0;
+    border: 1px solid ${grey[400]};
+    border-radius: 5px;
+    padding: 16px;
+    position: relative;
+
+    &::before {
+        color: ${grey[700]};
+        background-color: ${grey[50]};
+        content: 'フィルター設定';
+        font-size: 12px;
+        position: absolute;
+        padding: 4px 4px;
+        top: -14px;
+        left: 8px;
+    }
 `;
