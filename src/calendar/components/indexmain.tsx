8182a60c5cedf60c0f4eb6c9adaf746d9841ac2e
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-03-01 23:39:12 +0900

    カレンダーの表示を日本語に変更

--- src/calendar/components/indexmain.tsx
+++ src/calendar/components/indexmain.tsx
@@ -4,6 +4,9 @@ import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import { Box } from '@material-ui/core';
 
+const MONTHS = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'];
+const WEEKDAYS_SHORT = ['日', '月', '火', '水', '木', '金', '土'];
+
 export type Props = {
     selectedDate: Date;
     handleDayClicked: (day: any) => void;
@@ -13,7 +16,12 @@ export const IndexMain: React.FC<Props> = (props: Props) => {
     const { selectedDate, handleDayClicked } = props;
     return (
         <Box p={2}>
-            <DayPicker selectedDays={selectedDate} onDayClick={handleDayClicked} />
+            <DayPicker
+                selectedDays={selectedDate}
+                onDayClick={handleDayClicked}
+                months={MONTHS}
+                weekdaysShort={WEEKDAYS_SHORT}
+            />
         </Box>
     );
 };
