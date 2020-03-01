48a5f8f0ffebe53a409d30ee55930503afbd1b48
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-03-01 20:59:00 +0900

    ポップアップ画面表示時のちらつきを抑えるためにローディング画面の背景をprimaryにする

--- src/popup/components/loadingbox.tsx
+++ src/popup/components/loadingbox.tsx
@@ -3,8 +3,8 @@ import { CircularProgress, Box } from '@material-ui/core';
 
 export const LoadingBox: React.FC = () => {
     return (
-        <Box m={1}>
-            <CircularProgress size={16} />
+        <Box p={1} bgcolor="primary.main">
+            <CircularProgress size={16} color="secondary" />
         </Box>
     );
 };
