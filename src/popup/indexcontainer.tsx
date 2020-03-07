fc311e9d424584a7826a3586e6a0d70ec74039bf
Atsusuke <atsusuke-murata@cybozu.co.jp>
2020-03-08 01:04:26 +0900

    useCallbackとuseMemoを使うほどの関数ではないため使わない

--- src/popup/indexcontainer.tsx
+++ src/popup/indexcontainer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { FilterSetting } from 'src/types/storage';
 import { UserSettingServiceImpl } from '../storage/usersettingservice';
 import { IndexMain, Props as IndexProps } from './components/indexmain';
@@ -15,36 +15,33 @@ export const IndexContainer: React.FC<IndexContainerProps> = (props: IndexContai
     const [templateText, setTemplateText] = useState(initTemplateText);
     const [isPostMarkdown, setPostMarkdownFlag] = useState(initPostMarkdownFlag);
     const [openAlert, setOpenAlert] = useState(false);
-    const userSetting = useMemo(() => UserSettingServiceImpl.getInstance(), []);
+    const userSetting = UserSettingServiceImpl.getInstance();
 
-    const handleChangeFilterSettingSwitch = useCallback(
-        (name: string) => (event: React.ChangeEvent<HTMLInputElement>): void => {
-            setFilterSetting({ ...filterSetting, [name]: event.target.checked });
-        },
-        [filterSetting]
-    );
+    const handleChangeFilterSettingSwitch = (name: string) => (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setFilterSetting({ ...filterSetting, [name]: event.target.checked });
+    };
 
-    const handleChangePostMarkdownSwitch = useCallback((event: React.ChangeEvent<HTMLInputElement>): void => {
+    const handleChangePostMarkdownSwitch = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setPostMarkdownFlag(event.target.checked);
-    }, []);
+    };
 
-    const handleChangeText = useCallback((event: React.ChangeEvent<HTMLInputElement>): void => {
+    const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setTemplateText(event.target.value);
-    }, []);
+    };
 
-    const handleSaveBtnClick = useCallback(async (): Promise<void> => {
+    const handleSaveBtnClick = async (): Promise<void> => {
         setOpenAlert(true);
         await userSetting.setFilterSetting(filterSetting);
         await userSetting.setPostMarkdownFlag(isPostMarkdown);
         await userSetting.setTemplateText(templateText);
-    }, [filterSetting, isPostMarkdown, templateText]);
+    };
 
-    const handleCloseAlert = useCallback((event?: React.SyntheticEvent, reason?: string): void => {
+    const handleCloseAlert = (event?: React.SyntheticEvent, reason?: string): void => {
         if (reason === 'clickaway') {
             return;
         }
         setOpenAlert(false);
-    }, []);
+    };
 
     const indexProps: IndexProps = {
         isIncludePrivateEvent: filterSetting.isIncludePrivateEvent,
